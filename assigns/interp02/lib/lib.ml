43d9eb44a6c1b3fb0099c4cf2d2880f2c5736659
sobo3038 <sofiabl@bu.edu>
2024-11-21 14:08:31 -0500

    working on desugar

--- assigns/interp02/lib/lib.ml
+++ assigns/interp02/lib/lib.ml
@@ -30,7 +30,8 @@ let desugar (p : prog) : expr =
           (fun (arg_name, arg_ty) acc -> Fun (arg_name, arg_ty, acc))
           ((fst arg, snd arg) :: args)
           (desugar_sfexpr body)
-    | SApp (f, x) -> App (desugar_sfexpr f, desugar_sfexpr x)
+    | SApp (f, x) ->
+        App (desugar_sfexpr f, desugar_sfexpr x)
     | SLet { is_rec; name; args; ty; value; body } ->
         let desugared_value =
           List.fold_right
@@ -41,10 +42,13 @@ let desugar (p : prog) : expr =
         Let { is_rec; name; ty; value = desugared_value; body = desugar_sfexpr body }
     | SIf (cond, then_, else_) ->
         If (desugar_sfexpr cond, desugar_sfexpr then_, desugar_sfexpr else_)
-    | SBop (op, lhs, rhs) -> Bop (op, desugar_sfexpr lhs, desugar_sfexpr rhs)
-    | SAssert e -> Assert (desugar_sfexpr e)
+    | SBop (op, lhs, rhs) ->
+        Bop (op, desugar_sfexpr lhs, desugar_sfexpr rhs)
+    | SAssert e ->
+        Assert (desugar_sfexpr e)
   in
   desugar_toplets p
+
 
 (* Type-check*)
 let type_of (e : expr) : (ty, error) result =
@@ -151,7 +155,7 @@ let eval (e : expr) : value =
   in
   eval Env.empty e
 
-(* Interpreter *)
+(* Interpreter function *)
 let interp (s : string) : (value, error) result =
   match parse s with
   | None -> Error ParseErr
