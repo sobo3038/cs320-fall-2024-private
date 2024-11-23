810627dad5de5f2fbb20938c106cb99fed093319
sobo3038 <sofiabl@bu.edu>
2024-11-23 13:49:59 -0700

    idk

--- assigns/interp02/lib/lib.ml
+++ assigns/interp02/lib/lib.ml
@@ -1,208 +1,218 @@
 open Utils
 
-(* Parse a string into a program *)
-let parse (s : string) : prog option =
-  My_parser.parse s
+let parse = My_parser.parse
 
-(* Desugar a program into an expression *)
-let desugar (p : prog) : expr =
-  let rec desugar_toplets (toplets : toplet list) : expr =
-    match toplets with
-    | [] -> Unit
-    | { is_rec; name; args; ty; value } :: rest ->
-        let desugared_value =
-          List.fold_right
-            (fun (arg_name, arg_ty) acc -> Fun (arg_name, arg_ty, acc))
-            args
-            (desugar_sfexpr value)
-        in
-        let desugared_rest = desugar_toplets rest in
-        Let { is_rec; name; ty; value = desugared_value; body = desugared_rest }
-  and desugar_sfexpr (e : sfexpr) : expr =
-    match e with
-    | SUnit -> Unit
-    | STrue -> True
-    | SFalse -> False
-    | SNum n -> Num n
-    | SVar x -> Var x
-    | SFun { arg; args; body } ->
-        List.fold_right
-          (fun (arg_name, arg_ty) acc -> Fun (arg_name, arg_ty, acc))
-          ((fst arg, snd arg) :: args)
-          (desugar_sfexpr body)
-    | SApp (f, x) ->
-        App (desugar_sfexpr f, desugar_sfexpr x)
-    | SLet { is_rec; name; args; ty; value; body } ->
-        let desugared_value =
-          if is_rec then
-            Let { is_rec = true; name; ty;
-                  value =
-                    List.fold_right
-                      (fun (arg_name, arg_ty) acc -> Fun (arg_name, arg_ty, acc))
-                      args
-                      (desugar_sfexpr value);
-                  body = Var name }
-          else
+let desugar (prog : prog) : expr =
+    let rec desugar_toplets = function
+      | [] -> Unit
+      | { is_rec; name; args; ty; value } :: rest ->
+          let function_type =
             List.fold_right
-              (fun (arg_name, arg_ty) acc -> Fun (arg_name, arg_ty, acc))
+              (fun (_, arg_ty) acc -> FunTy(arg_ty, acc))
+              (args)
+              ty
+          in
+          let desugared_value =
+            List.fold_right
+              (fun (arg, arg_ty) acc -> Fun (arg, arg_ty, acc))
               args
-              (desugar_sfexpr value)
-        in
-        Let { is_rec; name; ty; value = desugared_value; body = desugar_sfexpr body }
-    | SIf (cond, then_, else_) ->
-        If (desugar_sfexpr cond, desugar_sfexpr then_, desugar_sfexpr else_)
-    | SBop (op, lhs, rhs) ->
-        Bop (op, desugar_sfexpr lhs, desugar_sfexpr rhs)
-    | SAssert e ->
-        Assert (desugar_sfexpr e)
-  in
-  desugar_toplets p
+              (desugar_expr value)
+          in
+          Let { is_rec; name; ty = function_type; value = desugared_value; body = desugar_toplets rest }
+    and desugar_expr = function
+      | SLet { is_rec; name; args; ty; value; body } ->
+          let function_type =
+            List.fold_right
+              (fun (_, arg_ty) acc -> FunTy(arg_ty, acc))
+              (args)
+              ty
+          in
+          let desugared_value =
+            List.fold_right
+              (fun (arg, arg_ty) acc -> Fun (arg, arg_ty, acc))
+              args
+              (desugar_expr value)
+          in
+          Let {
+            is_rec;
+            name;
+            ty = function_type;
+            value = desugared_value;
+            body = desugar_expr body;
+          }
+      | SFun { arg; args; body } ->
+          List.fold_right
+            (fun (arg, arg_ty) acc -> Fun (arg, arg_ty, acc))
+            (arg :: args)
+            (desugar_expr body)
+      | SIf (cond, then_, else_) ->
+          If (desugar_expr cond, desugar_expr then_, desugar_expr else_)
+      | SApp (e1, e2) ->
+          App (desugar_expr e1, desugar_expr e2)
+      | SBop (op, e1, e2) ->
+          Bop (op, desugar_expr e1, desugar_expr e2)
+      | SAssert e ->
+          Assert (desugar_expr e)
+      | SUnit -> Unit
+      | STrue -> True
+      | SFalse -> False
+      | SNum n -> Num n
+      | SVar x -> Var x
+    in
+    desugar_toplets prog
+  
 
-(* Type-check an expression *)
-let type_of (e : expr) : (ty, error) result =
-  let rec type_check (ctx : (string * ty) list) (e : expr) : (ty, error) result =
-    match e with
-    | Unit -> Ok UnitTy
-    | True | False -> Ok BoolTy
-    | Num _ -> Ok IntTy
-    | Var x -> (
-        match List.assoc_opt x ctx with
-        | Some ty -> Ok ty
-        | None -> Error (UnknownVar x))
-    | If (cond, then_, else_) -> (
-        match type_check ctx cond with
-        | Ok BoolTy -> (
-            match type_check ctx then_, type_check ctx else_ with
-            | Ok t1, Ok t2 when t1 = t2 -> Ok t1
-            | Ok t1, Ok t2 -> Error (IfTyErr (t1, t2))
-            | _, Error e | Error e, _ -> Error e)
-        | Ok ty -> Error (IfCondTyErr ty)
-        | Error e -> Error e)
-    | Bop (op, lhs, rhs) -> (
-        match type_check ctx lhs, type_check ctx rhs with
-        | Ok IntTy, Ok IntTy when op = Add || op = Sub || op = Mul || op = Div || op = Mod ->
-            Ok IntTy
-        | Ok IntTy, Ok IntTy when op = Lt || op = Lte || op = Gt || op = Gte || op = Eq || op = Neq ->
-            Ok BoolTy
-        | Ok BoolTy, Ok BoolTy when op = And || op = Or -> Ok BoolTy
-        | Ok l_ty, Ok r_ty ->
-            if l_ty <> IntTy then Error (OpTyErrL (op, IntTy, l_ty))
-            else Error (OpTyErrR (op, IntTy, r_ty))
-        | Error e, _ | _, Error e -> Error e)
-    | Fun (arg, arg_ty, body) -> (
-        match type_check ((arg, arg_ty) :: ctx) body with
-        | Ok body_ty -> Ok (FunTy (arg_ty, body_ty))
-        | Error e -> Error e)
-    | App (f, x) -> (
-        match type_check ctx f with
-        | Ok (FunTy (arg_ty, ret_ty)) -> (
-            match type_check ctx x with
-            | Ok ty when ty = arg_ty -> Ok ret_ty
-            | Ok ty -> Error (FunArgTyErr (arg_ty, ty))
-            | Error e -> Error e)
-        | Ok ty -> Error (FunAppTyErr ty)
-        | Error e -> Error e)
-    | Let { is_rec; name; ty; value; body } ->
-        let ctx' = if is_rec then (name, ty) :: ctx else ctx in
-        (match type_check ctx' value with
-        | Ok value_ty when value_ty = ty -> type_check ((name, ty) :: ctx) body
-        | Ok value_ty -> Error (LetTyErr (ty, value_ty))
-        | Error e -> Error e)
-    | Assert e -> (
-        match type_check ctx e with
-        | Ok BoolTy -> Ok UnitTy
-        | Ok ty -> Error (AssertTyErr ty)
-        | Error e -> Error e)
-  in
-  type_check [] e
- 
-
-
-
-(* Evaluation function exceptions *)
-exception AssertFail
 exception DivByZero
+exception AssertFail
+let type_of (expr : expr) : (ty, error) result =
+    let rec typecheck env expr =
+        match expr with
+        | Unit -> Ok UnitTy
+        | Num _ -> Ok IntTy
+        | True | False -> Ok BoolTy
+        | Var x -> (
+            match Env.find_opt x env with
+            | Some ty -> Ok ty
+            | None -> Error (UnknownVar x)
+        )
+        | Let { is_rec; name; ty = expected_ty; value; body } -> (
+            let extended_env =
+                if is_rec then Env.add name expected_ty env else env
+            in
+            match typecheck extended_env value with
+            | Ok actual_ty ->
+                if actual_ty = expected_ty then
+                    typecheck (Env.add name expected_ty extended_env) body
+                else Error (LetTyErr (expected_ty, actual_ty))
+            | Error e -> Error e
+        )
+        | Fun (arg, arg_ty, body) ->
+            let extended_env = Env.add arg arg_ty env in
+            (match typecheck extended_env body with
+            | Ok body_ty -> Ok (FunTy (arg_ty, body_ty))
+            | Error e -> Error e)
+        | App (e1, e2) -> (
+            match typecheck env e1, typecheck env e2 with
+            | Error e, _ -> Error e
+            | _, Error e -> Error e
+            | Ok (FunTy (arg_ty, ret_ty)), Ok actual_ty ->
+                if arg_ty = actual_ty then Ok ret_ty
+                else Error (FunArgTyErr (arg_ty, actual_ty))
+            | Ok ty, _ -> Error (FunAppTyErr ty)
+        )
+        | If (cond, then_, else_) -> (
+            match typecheck env cond with
+            | Ok BoolTy -> (
+                let ty_then = typecheck env then_ in
+                let ty_else = typecheck env else_ in
+                match ty_then, ty_else with
+                | Ok ty_then, Ok ty_else when ty_then = ty_else -> Ok ty_then
+                | Ok ty_then, Ok ty_else -> Error (IfTyErr (ty_then, ty_else))  
+                | Error e, _ -> Error e
+                | _, Error e -> Error e
+            )
+            | Ok ty -> Error (IfCondTyErr ty)
+            | Error e -> Error e
+        )
+        | Bop (op, e1, e2) -> (
+            match typecheck env e1, typecheck env e2 with
+            | Ok ty1, Ok ty2 -> (
+                match op with
+                | Add | Sub | Mul | Div | Mod when ty1 = IntTy && ty2 = IntTy -> Ok IntTy
+                | Lt | Lte | Gt | Gte | Eq | Neq when ty1 = IntTy && ty2 = IntTy -> Ok BoolTy
+                | And | Or when ty1 = BoolTy && ty2 = BoolTy -> Ok BoolTy
+                | Add | Sub | Mul | Div | Mod when ty1 <> IntTy -> Error (OpTyErrL (op, IntTy, ty1))
+                | Add | Sub | Mul | Div | Mod when ty2 <> IntTy -> Error (OpTyErrR (op, IntTy, ty2))
+                | Lt | Lte | Gt | Gte | Eq | Neq when ty1 <> IntTy -> Error (OpTyErrL (op, IntTy, ty1))
+                | Lt | Lte | Gt | Gte | Eq | Neq when ty2 <> IntTy -> Error (OpTyErrR (op, IntTy, ty2))
+                | And | Or when ty1 <> BoolTy -> Error (OpTyErrL (op, BoolTy, ty1))
+                | And | Or when ty2 <> BoolTy -> Error (OpTyErrR (op, BoolTy, ty2))
+                | _ -> Error (OpTyErrL (op, ty1, ty2))
+            )
+            | Error e, _ -> Error e
+            | _, Error e -> Error e
+        )
+        | Assert e -> (
+            match typecheck env e with
+            | Ok BoolTy -> Ok UnitTy
+            | Ok ty -> Error (AssertTyErr ty)
+            | Error e -> Error e
+        )
+    in
+    typecheck Env.empty expr
 
-(* Evaluate an expression *)
-let eval (e : expr) : value =
-  let rec eval env e =
-    match e with
-    | Unit -> VUnit
-    | True -> VBool true
-    | False -> VBool false
-    | Num n -> VNum n
-    | Var x -> Env.find x env
-    | If (cond, then_, else_) ->
-        let v_cond = eval env cond in
-        begin
-          match v_cond with
-          | VBool true -> eval env then_
-          | VBool false -> eval env else_
-          | _ -> raise (Failure "Type error in if condition")
-        end
-    | Bop (op, lhs, rhs) ->
-        let v1 = eval env lhs in
-        let v2 = eval env rhs in
-        begin
-          match (op, v1, v2) with
-          | (Add, VNum n1, VNum n2) -> VNum (n1 + n2)
-          | (Sub, VNum n1, VNum n2) -> VNum (n1 - n2)
-          | (Mul, VNum n1, VNum n2) -> VNum (n1 * n2)
-          | (Div, VNum n1, VNum n2) ->
-              if n2 = 0 then raise DivByZero else VNum (n1 / n2)
-          | (Mod, VNum n1, VNum n2) ->
-              if n2 = 0 then raise DivByZero else VNum (n1 mod n2)
-          | (Lt, VNum n1, VNum n2) -> VBool (n1 < n2)
-          | (Lte, VNum n1, VNum n2) -> VBool (n1 <= n2)
-          | (Gt, VNum n1, VNum n2) -> VBool (n1 > n2)
-          | (Gte, VNum n1, VNum n2) -> VBool (n1 >= n2)
-          | (Eq, VNum n1, VNum n2) -> VBool (n1 = n2)
-          | (Neq, VNum n1, VNum n2) -> VBool (n1 <> n2)
-          | (And, VBool b1, VBool b2) -> VBool (b1 && b2)
-          | (Or, VBool b1, VBool b2) -> VBool (b1 || b2)
-          | _ -> raise (Failure "Type error in binary operation")
-        end
-    | Fun (arg, _, body) -> VClos { name = None; arg; body; env }
-    | App (f, x) ->
-        let v_f = eval env f in
-        let v_x = eval env x in
-        begin
-          match v_f with
-          | VClos { name = _; arg; body; env = closure_env } ->
-              let env' = Env.add arg v_x closure_env in
-              eval env' body
-          | _ -> raise (Failure "Type error in function application")
-        end
-    | Let { is_rec; name; ty = _; value; body } -> (* `ty` is ignored for evaluation *)
-        let value_v =
+
+let eval (expr : expr) : value =
+    let rec eval_expr env expr =
+      match expr with
+      | Unit -> VUnit
+      | Num n -> VNum n
+      | True -> VBool true
+      | False -> VBool false
+      | Var x -> Env.find x env
+      | Let { is_rec; name; ty = _; value; body } ->
+        let closure_env =
           if is_rec then
-            VClos { name = Some name; arg = ""; body = value; env }
-          else eval env value
+            match value with
+            | Fun (arg, _, body) ->
+                Env.add name (VClos { name = Some name; arg; body; env }) env
+            | _ ->
+                Env.add name (VClos { name = Some name; arg = ""; body = value; env }) env
+          else env
         in
-        let env' = Env.add name value_v env in
-        eval env' body
-    | Assert e ->
-        let v = eval env e in
-        begin
-          match v with
-          | VBool true -> VUnit
-          | _ -> raise AssertFail
-        end
-  in
-  eval Env.empty e (* Assuming `Env.empty` initializes an empty environment *)
+        let v = eval_expr closure_env value in
+        let extended_env = Env.add name v closure_env in
+        eval_expr extended_env body    
+      | Fun (arg, _, body) -> VClos { name = None; arg; body; env }
+      | App (e1, e2) -> (
+        match eval_expr env e1 with
+        | VClos { name = _; arg; body; env = closure_env } ->
+            let v2 = eval_expr env e2 in
+            let extended_env = Env.add arg v2 closure_env in
+            eval_expr extended_env body
+        | _ -> assert false
+    )
+        | If (cond, then_, else_) -> (
+            match eval_expr env cond with
+            | VBool b ->
+                if b then eval_expr env then_ else eval_expr env else_
+            | _ -> assert false
+        )
+      | Bop (op, e1, e2) ->
+          let v1 = eval_expr env e1 in
+          let v2 = eval_expr env e2 in
+          begin match (v1, v2, op) with
+          | VNum n1, VNum n2, Add -> VNum (n1 + n2)
+          | VNum n1, VNum n2, Sub -> VNum (n1 - n2)
+          | VNum n1, VNum n2, Mul -> VNum (n1 * n2)
+          | VNum n1, VNum n2, Div -> if n2 = 0 then raise DivByZero else VNum (n1 / n2)
+          | VNum n1, VNum n2, Mod -> if n2 = 0 then raise DivByZero else VNum (n1 mod n2)
+          | VNum n1, VNum n2, Lt -> VBool (n1 < n2)
+          | VNum n1, VNum n2, Lte -> VBool (n1 <= n2)
+          | VNum n1, VNum n2, Gt -> VBool (n1 > n2)
+          | VNum n1, VNum n2, Gte -> VBool (n1 >= n2)
+          | VNum n1, VNum n2, Eq -> VBool (n1 = n2)
+          | VNum n1, VNum n2, Neq -> VBool (n1 <> n2)
+          | VBool b1, VBool b2, And -> VBool (b1 && b2)
+          | VBool b1, VBool b2, Or -> VBool (b1 || b2)
+          | _ -> assert false
+          end
+        | Assert e -> (
+        match eval_expr env e with
+        | VBool b ->
+            if b then VUnit else raise AssertFail
+        | _ -> assert false
+        )
+    in
+    eval_expr Env.empty expr
+  
 
-
-(* Interpreter function *)
-
-let interp (s : string) : (value, error) result =
-  match parse s with
-  | None -> Error ParseErr
-  | Some prog -> (
-      let desugared = desugar prog in
-      match type_of desugared with
-      | Error e -> Error e
-      | Ok _ -> Ok (eval desugared))
- 
- 
- 
- 
+  let interp (input : string) : (value, error) result =
+    match parse input with
+    | Some prog -> (
+        let expr = desugar prog in
+        match type_of expr with
+        | Ok _ -> Ok (eval expr)
+        | Error e -> Error e
+    )
+    | None -> Error ParseErr
--- assigns/interp02/lib/parser/par.mly
+++ assigns/interp02/lib/parser/par.mly
@@ -1,116 +1,109 @@
-
 %{
 open Utils
 %}
 
-
-%token <int> NUM
-%token <string> VAR
 %token EOF
-%token LET IN REC EQ COLON
-%token IF THEN ELSE
-%token FUN ARROW
-%token ASSERT
-%token MOD
-%token TRUE FALSE UNIT
-%token LPAREN RPAREN
-%token PLUS MINUS TIMES DIV
-%token LT LTE GT GTE NEQ
-%token AND OR
-%token INT BOOL
+%token FUN "fun"
+%token ARROW "->"
+%token LPAREN "("
+%token RPAREN ")"
+%token LET "let"
+%token REC "rec"
+%token EQ "="
+%token DIV "/"
+%token MOD "mod"
+%token LT "<"
+%token LTE "<="
+%token GT ">"
+%token GTE ">="
+%token NEQ "<>"
+%token AND "&&"
+%token OR "||"
+%token IN "in"
+%token <string> VAR
+%token <int> NUM
+%token IF "if"
+%token THEN "then"
+%token ELSE "else"
+%token ADD "+"
+%token MUL "*"
+%token SUB "-"
+%token TRUE "true"
+%token FALSE "false"
+%token ASSERT "assert"
 
+%token INTTY "int"
+%token BOOLTY "bool"
+%token COLON ":"
+
+%token UNIT "()"
+%token UNITTY "unit"
+
+%right OR
+%right AND
+%left LT LTE GT GTE EQ NEQ
+%left ADD SUB
+%left MUL DIV MOD
 
 %start <Utils.prog> prog
 
-
 %%
 
-
 prog:
-   toplet_list EOF { $1 }
-
-
-toplet_list:
-   toplet toplet_list { $1 :: $2 }
- | /* empty */        { [] }
-
+  | ls = toplet * EOF { ls }
 
 toplet:
-   LET VAR args_opt COLON ty EQ expr { { is_rec = false; name = $2; args = $3; ty = $5; value = $7 } }
- | LET REC VAR args_opt COLON ty EQ expr { { is_rec = true; name = $3; args = $4; ty = $6; value = $8 } }
+  | "let" x = VAR args = arg* ":" ty = ty "=" e = expr
+  { { is_rec = false; name = x; args = args; ty; value = e } }
+  | "let" "rec" x = VAR arg = arg ; args = arg* ":" ty = ty "=" e = expr
+  { { is_rec = true; name = x; args = arg :: args; ty; value = e } }
 
-
-args_opt:
-   LPAREN VAR COLON ty RPAREN args_opt { ($2, $4) :: $6 }
- | /* empty */                     { [] }
-
+arg:
+  | "(" x = VAR ":" ty = ty ")" { (x, ty) }
 
 ty:
-   INT                             { IntTy }
- | BOOL                            { BoolTy }
- | UNIT                            { UnitTy }
- | ty ARROW ty                     { FunTy ($1, $3) }
- | LPAREN ty RPAREN                { $2 }
-
+  | "int" { IntTy }
+  | "bool" { BoolTy }
+  | "unit" { UnitTy }
+  | t1 = ty "->" t2 = ty { FunTy(t1, t2) }
+  | "(" t = ty ")" { t }
 
 expr:
-   LET VAR COLON ty EQ expr IN expr { SLet { is_rec = false; name = $2; args = []; ty = $4; value = $6; body = $8 } }
- | LET REC VAR args_opt COLON ty EQ expr IN expr { SLet { is_rec = true; name = $3; args = $4; ty = $6; value = $8; body = $10 } }
- | IF expr THEN expr ELSE expr { SIf ($2, $4, $6) }
- | ASSERT expr                 { SAssert ($2) }
- | FUN VAR COLON ty ARROW expr { SFun { arg = ($2, $4); args = []; body = $6 } }
- | expr_or                     { $1 }
+  | "let" x = VAR args = arg* ":" ty = ty "=" e1 = expr "in" e2 = expr
+  { SLet { is_rec = false; name = x; args = args; ty; value = e1; body = e2 } }
+  | "let" "rec" x = VAR arg = arg ; args = arg* ":" ty = ty "=" e1 = expr "in" e2 = expr
+  { SLet { is_rec = true; name = x; args = arg :: args; ty; value = e1; body = e2 } }
+  | "if" e1 = expr "then" e2 = expr "else" e3 = expr
+  { SIf(e1, e2, e3) }
+  | "fun" arg = arg ; args = arg* "->" e = expr
+  { SFun { arg = arg; args = args; body = e } }
+  | e = expr2 { e } 
+
+%inline bop:
+  | ADD { Add }
+  | SUB { Sub }
+  | MUL { Mul }
+  | DIV { Div }
+  | MOD { Mod }
+  | LT { Lt }
+  | LTE { Lte }
+  | GT { Gt }
+  | GTE { Gte }
+  | EQ { Eq }
+  | NEQ { Neq }
+  | AND { And }
+  | OR { Or }
+
+expr2:
+  | e1 = expr2; op = bop; e2 = expr2 { SBop (op, e1, e2) }
+  | "assert" e = expr3 { SAssert e }
+  | e1 = expr3 es = expr3* { List.fold_left (fun e1 e2 -> SApp(e1, e2)) e1 es }
 
 
-expr_or:
-   expr_or OR expr_and         { SBop (Or, $1, $3) }
- | expr_and                    { $1 }
-
-
-expr_and:
-   expr_and AND expr_rel       { SBop (And, $1, $3) }
- | expr_rel                    { $1 }
-
-
-expr_rel:
-   expr_rel relop expr_add     { SBop ($2, $1, $3) }
- | expr_add                    { $1 }
-
-
-relop:
-   LT  { Lt }
- | LTE { Lte }
- | GT  { Gt }
- | GTE { Gte }
- | EQ  { Eq }
- | NEQ { Neq }
-
-
-expr_add:
-   expr_add PLUS expr_mul      { SBop (Add, $1, $3) }
- | expr_add MINUS expr_mul     { SBop (Sub, $1, $3) }
- | expr_mul                    { $1 }
-
-
-expr_mul:
-   expr_mul TIMES expr_app     { SBop (Mul, $1, $3) }
- | expr_mul DIV expr_app       { SBop (Div, $1, $3) }
- | expr_mul MOD expr_app       { SBop (Mod, $1, $3) }
- | expr_app                    { $1 }
-
-
-expr_app:
-   expr_app expr_atomic        { SApp ($1, $2) }
- | expr_atomic                 { $1 }
-
-
-expr_atomic:
-   UNIT                        { SUnit }
- | TRUE                        { STrue }
- | FALSE                       { SFalse }
- | NUM                         { SNum $1 }
- | VAR                         { SVar $1 }
- | LPAREN expr RPAREN          { $2 }
-
-
-
+expr3:
+  | UNIT { SUnit }
+  | TRUE { STrue }
+  | FALSE { SFalse }
+  | n = NUM { SNum n }
+  | x = VAR { SVar x }
+  | "(" e = expr ")" { e }
